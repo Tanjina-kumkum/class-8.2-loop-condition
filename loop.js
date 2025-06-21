247f3b563521c420baf090f6a282fdb63ec1927c
Tanjina-kumkum <tanjinaakter882@gmail.com>
2025-06-21 23:01:56 +0600

    some are added

--- loop.js
+++ loop.js
@@ -23,3 +23,44 @@ for(f=0;f<numbers2.length;f++){
         
     }
 }
+
+let h=0
+while(h<10){
+    h++
+    console.log(h);
+    
+    h++
+}
+
+let numbers3=[11,12,13,14,15]
+var sum1=0
+var k=0
+while(k<numbers3.length){
+  sum1= sum1 + numbers3[k]  
+  console.log(sum1);
+ k=k+1 
+}
+let num=[22,23,24,25,26]
+
+var j=0
+while(j<num.length){
+if(num[j]%2==0){
+    console.log(`${num[j]}`);
+    
+}
+j++
+}
+
+let num1=[31,32,33,34,35,36,37,38,39,40]
+var l=0
+while(l<num1.length){
+if(num1[l%2!=0]){
+    console.log(`${num1[l]}`);
+    
+}
+
+    l++
+}
+
+
+
