5d7a410920c1c7c71aa9998f263f1c29f2a97116
Tanjina-kumkum <tanjinaakter882@gmail.com>
2025-06-21 21:58:46 +0600

    first commit

---
+++ loop.js
@@ -0,0 +1,11 @@
+for(i=0; i<10; i++){
+    console.log(i);
+    
+}
+const numbers=[10,11,12,13,14,15,16,17,18,19,20];
+var sum=0
+for(x=0; x<numbers.length; x++){
+    sum= sum + numbers[x]
+    console.log(sum);
+    
+}
