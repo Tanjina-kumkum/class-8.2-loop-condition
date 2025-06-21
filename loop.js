a73c6a50a24d3562b8f2715b9f948e31d89287b0
Tanjina-kumkum <tanjinaakter882@gmail.com>
2025-06-21 22:11:57 +0600

    some are added

--- loop.js
+++ loop.js
@@ -9,3 +9,17 @@ for(x=0; x<numbers.length; x++){
     console.log(sum);
     
 }
+let numbers1=[21,22,23,24,25,26,27,28,29,30]
+for(g=0;g<numbers1.length;g++){
+    if(numbers1[g]%2==0){
+        console.log(`${numbers1[g]}`);
+        
+    }
+}
+let numbers2=[31,32,33,34,35,36,37,38,39,40];
+for(f=0;f<numbers2.length;f++){
+    if(numbers2[f]%2!=0){
+        console.log(`${numbers2[f]}`);
+        
+    }
+}
